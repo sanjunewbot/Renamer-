8c5112cca02b128b7c9389578cd220ac43d8379c
sanjunewbot <lavotatunaxa79@gmail.com>
2026-06-24 02:20:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import asyncio
 from pyrogram import Client, idle
 from config import API_ID, API_HASH, BOT_TOKEN
+from webserver import start_webserver
 
 app = Client(
     "RenameBot",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="handlers")
 )
 
-if __name__ == "__main__":
-    app.start()
+async def main():
+    await start_webserver()
+    await app.start()
     print("Bot Started")
-    idle()
-    app.stop()
+    await idle()
+    await app.stop()
+
+app.run(main())
