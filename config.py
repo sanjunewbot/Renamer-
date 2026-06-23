cf3a040fd19135634a2fddd5f54ebbb9cc16b528
sanjunewbot <lavotatunaxa79@gmail.com>
2026-06-24 02:04:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("20342933"))
+API_HASH = os.getenv("9233e5deebe6abfc9ba297a9678851be")
+BOT_TOKEN = os.getenv("8434987797:AAEPMRsydRT81ZqylwjbwFsNwYkQPQ2Z1tA")
 
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = os.getenv("mongodb+srv://sanjusen212121:dRTWTRgVX19SJwTk@cluster0.2nwk3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+")
 
-ADMINS = [int(x) for x in os.getenv("ADMINS", "").split()]
+ADMINS = [int(x) for x in os.getenv("6803963354", "").split()]
 
 DB_NAME = "RenameBot"
 
